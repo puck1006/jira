42fa679d058392ce092ffdb2f4be9c192a29e2dc
spicycurry <18017686817@163.com>
2021-07-31 07:36:46 +0800

    ci: add commitlint final version

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import logo from "logo.svg";
 import "App.css";
 
 function App() {
-  console.log("test git commitlint");
   return (
     <div className="App">
       <header className="App-header">
