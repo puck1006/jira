babc7f2448e2b876a84494d47d617e2752293b38
spicycurry <18017686817@163.com>
2021-07-31 07:34:22 +0800

    ci: add commitlint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import logo from "logo.svg";
 import "App.css";
 
 function App() {
-  console.log("test git");
+  console.log("test git commitlint");
   return (
     <div className="App">
       <header className="App-header">
