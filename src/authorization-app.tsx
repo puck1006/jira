2d9001e9c2ea8d61ca87f588c2e9d09f2632e776
spicycurry <18017686817@163.com>
2021-08-04 07:59:40 +0800

    feat: 用flex和grid 布局页面,并且抽象布局组件

--- src/authorization-app.tsx
+++ src/authorization-app.tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+import { Row } from "component/lib";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
 
@@ -5,15 +7,41 @@ export const AuthorizationApp = () => {
   const { logout } = useAuth();
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          logout();
-        }}
-      >
-        登出
-      </button>
-      <ProjectListScreen />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h3>Logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button
+            onClick={() => {
+              logout();
+            }}
+          >
+            登出
+          </button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListScreen />
+      </Main>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr 6rem;
+  height: 100vh;
+`;
+
+const Header = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+
+const Main = styled.div`
+  height: calc(100vh - 6rem);
+`;
