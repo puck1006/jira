59e9153ee02b743de0aa43bcab4b6fd511094de9
spicycurry <18017686817@163.com>
2021-08-07 10:09:56 +0800

    feat: 封装id-select组件

---
+++ src/component/id-select.tsx
@@ -0,0 +1,35 @@
+import { Select } from "antd";
+import { Raw } from "types";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: { name: string; id: number }[];
+}
+
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restprops } = props;
+
+  return (
+    <Select
+      {...restprops}
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((item) => (
+        <Select.Option value={item.id} key={item.id}>
+          {item.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
